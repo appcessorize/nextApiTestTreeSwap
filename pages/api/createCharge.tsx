6d7746c1d5687e65accf72b0caf9f1f2cd423833
Matt <matt@Matts-MacBook-Air.local>
2022-11-12 22:15:08 +0000

    add cors options

--- pages/api/createCharge.tsx
+++ pages/api/createCharge.tsx
@@ -10,6 +10,7 @@ import Cors from "cors";
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
   methods: ["POST", "GET", "HEAD"],
+  origin: "*",
 });
 
 // Helper method to wait for a middleware to execute before continuing
